37059eed8ca4315f29d8aa367749cf3cfbaf2bd3
Xavier BURKE <xavier.burke@openpixl.fr>
2021-08-11 09:23:22 +0200

    Adaptation template CkEditor

--- src/Form/Admin/AccompagnementType.php
+++ src/Form/Admin/AccompagnementType.php
@@ -3,7 +3,11 @@
 namespace App\Form\Admin;
 
 use App\Entity\Admin\Accompagnement;
+use App\Entity\Admin\TypeAcc;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,18 @@ class AccompagnementType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('TypeAcc')
-            ->add('description')
-        ;
+            ->add('TypeAcc', EntityType::class, [
+                'class' => TypeAcc::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('t')
+                        ->orderBy('t.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                'attr' => ['class' => 'form-control form-select']
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => ['class' => 'form-control', 'rows' => 10],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
